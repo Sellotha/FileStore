639d5651376836dd6e8b85a0a3e118fe69d8fd76
Sellotha <laptop7033@gmail.com>
2025-07-24 17:39:38 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -42,11 +42,12 @@ async def start_command(client: Client, message: Message):
                 [[InlineKeyboardButton("Contact Support", url=BAN_SUPPORT)]]
             )
         )
-    # ✅ Check Force Subscription
-    if not await is_subscribed(client, user_id):
-        #await temp.delete()
-        return await not_joined(client, message)
 
+    # ✅ Skip Force Sub for plain /start
+    if len(message.command) > 1:
+        if not await is_subscribed(client, user_id):
+            return await not_joined(client, message)        
+            
     # File auto-delete time in seconds (Set your desired time in seconds here)
     FILE_AUTO_DELETE = await db.get_del_timer()  # Example: 3600 seconds (1 hour)
 
